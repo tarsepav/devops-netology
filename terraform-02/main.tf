79e982aa7d5e4354fff309f117986e515646ddef
tarsepav <94912676+tarsepav@users.noreply.github.com>
2022-08-05 18:41:49 +0300

    Update main.tf

--- terraform-02/main.tf
+++ terraform-02/main.tf
@@ -18,3 +18,14 @@ resource "yandex_compute_instance" "node01" {
     }
   }
 
+resource "yandex_vpc_network" "default" {
+  name = "net"
+}
+
+resource "yandex_vpc_subnet" "default" {
+  name           = "subnet"
+  zone           = "ru-central1-b"
+  network_id     = "${yandex_vpc_network.default.id}"
+  v4_cidr_blocks = ["192.168.101.0/24"]
+}
+
