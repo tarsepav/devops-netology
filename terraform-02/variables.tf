76fa635da3348736f6cdb87767c8f5f853a34f9f
tarsepav <94912676+tarsepav@users.noreply.github.com>
2022-08-05 18:38:57 +0300

    Create variables.tf

---
+++ terraform-02/variables.tf
@@ -0,0 +1,11 @@
+variable "yandex_cloud_id" {
+  default = "b1givpq4toj7vjaqq8dq"
+}
+
+variable "yandex_folder_id" {
+  default = "b1gl0pc5s6eunge99bo8"
+}
+
+variable "centos-7-base" {
+  default = "fd8lfipq13fdfjisoe32"
+}
