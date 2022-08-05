aaa8d510d0d917637c05cb4c750db9be836ff452
tarsepav <94912676+tarsepav@users.noreply.github.com>
2022-08-05 18:38:32 +0300

    Create main.tf

---
+++ terraform-02/main.tf
@@ -0,0 +1,20 @@
+resource "yandex_compute_instance" "node01" {
+  name                      = "node01"
+  zone                      = "ru-central1-a"
+  hostname                  = "node01.netology.cloud"
+  allow_stopping_for_update = true
+
+  resources {
+    cores  = 2
+    memory = 2
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id    = "${var.centos-7-base}"
+      name        = "root-node01"
+      type        = "network-nvme"
+      size        = "50"
+    }
+  }
+
